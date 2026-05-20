a66b4437232a4e0a128dbeb4ad1305cb17f65877
S-N-Onika <sumsunnaharonika12@gmail.com>
2026-05-20 19:10:06 +0600

    Footer design completed

--- studynook-client/src/layouts/MainLayout.jsx
+++ studynook-client/src/layouts/MainLayout.jsx
@@ -1,19 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
-// import Footer from "../components/Footer"; 
+import Footer from "../components/Footer";
 
 export default function MainLayout() {
     return (
-        <div className="flex flex-col min-h-screen bg-slate-50 text-slate-900 font-sans antialiased">
-
+        <div className="flex flex-col min-h-screen bg-[#FBF8F3] text-slate-900 font-sans antialiased">
             <Navbar />
-
             <main className="flex-grow">
                 <Outlet />
             </main>
-
-            
-            {/* <Footer /> */}
+            <Footer />
         </div>
     );
 }
